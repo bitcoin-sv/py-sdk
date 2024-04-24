5393a59d86392f3e01f20d3f51fabc4c32d1b2b0
Yusuf Idi Maina <120538505+yusufidimaina9989@users.noreply.github.com>
2024-04-24 13:53:59 +0100

    Add hmac

--- bsv/hash.py
+++ bsv/hash.py
@@ -1,4 +1,5 @@
 import hashlib
+import hmac
 
 from Cryptodome.Hash import RIPEMD160
 
@@ -17,3 +18,11 @@ def ripemd160_sha256(payload: bytes) -> bytes:
 
 hash256 = double_sha256
 hash160 = ripemd160_sha256
+
+
+def hmac_sha256(key: bytes, message: bytes) -> bytes:
+    return hmac.new(key, message, hashlib.sha256).digest()
+
+
+def hmac_sha512(key: bytes, message: bytes) -> bytes:
+    return hmac.new(key, message, hashlib.sha512).digest()
--- tests/test_hash.py
+++ tests/test_hash.py
@@ -1,4 +1,4 @@
-from bsv.hash import sha256, double_sha256, ripemd160_sha256
+from bsv.hash import sha256, double_sha256, ripemd160_sha256, hmac_sha256, hmac_sha512
 
 MESSAGE = 'hello'.encode('utf-8')
 MESSAGE_SHA256 = bytes.fromhex('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
@@ -16,3 +16,17 @@ def test_double_sha256():
 
 def test_ripemd160_sha256():
     assert ripemd160_sha256(MESSAGE) == MESSAGE_HASH160
+
+
+KEY = 'key'.encode('utf-8')
+MESSAGE_HMAC_SHA256 = bytes.fromhex('9307b3b915efb5171ff14d8cb55fbcc798c6c0ef1456d66ded1a6aa723a58b7b')
+MESSAGE_HMAC_SHA512 = bytes.fromhex('ff06ab36757777815c008d32c8e14a705b4e7bf310351a06a23b612dc4c7433e\
+                                     7757d20525a5593b71020ea2ee162d2311b247e9855862b270122419652c0c92')
+
+
+def test_hmac_sha256():
+    assert hmac_sha256(KEY, MESSAGE) == MESSAGE_HMAC_SHA256
+
+
+def test_hmac_sha512():
+    assert hmac_sha512(KEY, MESSAGE) == MESSAGE_HMAC_SHA512
