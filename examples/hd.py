4cef1e2a3d4361fbc422e1aa53eb68565a349541
msinkec <sinkec.mihael@gmail.com>
2024-08-05 08:57:14 +0200

    expand hd example

--- examples/hd.py
+++ examples/hd.py
@@ -20,7 +20,17 @@ print(master_xprv)
 print()
 keys: List[Xprv] = derive_xprvs_from_mnemonic(mnemonic, path="m/44'/0'/0'", change=1, index_start=0, index_end=5)
 for key in keys:
-    print(key.address(), key.private_key().wif())
+    # XPriv to WIF
+    print(key.private_key().wif())
+
+    key_xpub = key.xpub()
+
+    # XPub to public key
+    print(key_xpub.public_key().hex())
+
+    # XPub to address
+    print(key_xpub.public_key().address(), '\n')
+
 
 #
 # random mnemonic
