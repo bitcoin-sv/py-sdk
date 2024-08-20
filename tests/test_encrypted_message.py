e3cf20bb1139a696549379966418b59f64fd1d56
msinkec <sinkec.mihael@gmail.com>
2024-08-20 14:56:28 +0200

    Wrap functions into SignedMessage and EncryptedMessage classes.

--- tests/test_encrypted_message.py
+++ tests/test_encrypted_message.py
@@ -1,6 +1,6 @@
 import pytest
 
-from bsv.encrypted_message import aes_gcm_encrypt, aes_gcm_decrypt, encrypt, decrypt
+from bsv.encrypted_message import EncryptedMessage
 from bsv.keys import PrivateKey
 from bsv.utils import randbytes
 
@@ -8,21 +8,21 @@ from bsv.utils import randbytes
 def test_aes_gcm():
     key = randbytes(32)
     message = 'hello world'.encode('utf-8')
-    encrypted = aes_gcm_encrypt(key, message)
-    decrypted = aes_gcm_decrypt(key, encrypted)
+    encrypted = EncryptedMessage.aes_gcm_encrypt(key, message)
+    decrypted = EncryptedMessage.aes_gcm_decrypt(key, encrypted)
     assert decrypted == message
 
 
 def test_brc78():
     message = 'hello world'.encode('utf-8')
     sender_priv, recipient_priv = PrivateKey(), PrivateKey()
-    encrypted = encrypt(message, sender_priv, recipient_priv.public_key())
-    decrypted = decrypt(encrypted, recipient_priv)
+    encrypted = EncryptedMessage.encrypt(message, sender_priv, recipient_priv.public_key())
+    decrypted = EncryptedMessage.decrypt(encrypted, recipient_priv)
     assert decrypted == message
 
     with pytest.raises(ValueError, match=r'message version mismatch'):
-        decrypt(encrypted[1:], PrivateKey())
+        EncryptedMessage.decrypt(encrypted[1:], PrivateKey())
     with pytest.raises(ValueError, match=r'recipient public key mismatch'):
-        decrypt(encrypted, PrivateKey())
+        EncryptedMessage.decrypt(encrypted, PrivateKey())
     with pytest.raises(ValueError, match=r'failed to decrypt message'):
-        decrypt(encrypted[:-1], recipient_priv)
+        EncryptedMessage.decrypt(encrypted[:-1], recipient_priv)
