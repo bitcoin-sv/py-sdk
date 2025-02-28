fab9343e79c8a3afd11fc4dd8813b64b5483fc43
kensato <ken16310@gmail.com>
2025-02-28 18:09:00 +0900

    update the key dervation examples

--- bsv/hd/__init__.py
+++ bsv/hd/__init__.py
@@ -1,3 +1,5 @@
-from .bip32 import Xkey, Xprv, Xpub, ckd, step_to_index, master_xprv_from_seed
+from .bip32 import Xkey, Xprv, Xpub, ckd, step_to_index, master_xprv_from_seed, bip32_derive_xprv_from_mnemonic, \
+    bip32_derive_xprvs_from_mnemonic, bip32_derive_xkeys_from_xkey
 from .bip39 import WordList, mnemonic_from_entropy, seed_from_mnemonic, validate_mnemonic
-from .bip44 import derive_xkeys_from_xkey, derive_xprvs_from_mnemonic, derive_xprv_from_mnemonic
+from .bip44 import derive_xkeys_from_xkey, derive_xprvs_from_mnemonic, derive_xprv_from_mnemonic, \
+    bip44_derive_xprv_from_mnemonic, bip44_derive_xprvs_from_mnemonic
